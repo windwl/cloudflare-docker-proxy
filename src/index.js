bace3cfbc3a5b30b26a61e9407515ed59c359597
leo <windwl@126.com>
2024-07-27 22:07:20 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tingxi.wang": dockerHub,
+  "quay.tingxi.wang": "https://quay.io",
+  "gcr.tingxi.wang": "https://gcr.io",
+  "k8s-gcr.tingxi.wang": "https://k8s.gcr.io",
+  "k8s.tingxi.wango": "https://registry.k8s.io",
+  "ghcr.tingxi.wang": "https://ghcr.io",
+  "cloudsmith.tingxi.wang": "https://docker.cloudsmith.io",
+  "ecr.tingxi.wang": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tingxi.wang": dockerHub,
 };
 
 function routeByHosts(host) {
